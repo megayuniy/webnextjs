30fbc726371024144ea1136d5fe50faca439b341
megayuniy <meyuyume@gmail.com>
2024-12-16 18:57:13 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold">About Page</h1>
+
+        <a href="http://localhost:3000/" className="text-blue-500 underline">Home</a>
+    </div>
+  )
+}
+
+export default About
